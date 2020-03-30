83fa32c360b769d60cd590c27fdb59ebbb8930a6
Shamith Achanta <43729418+shamith2@users.noreply.github.com>
2020-03-30 06:39:04 -0500

    Update runBO.py

--- fmfnBO/runBO.py
+++ fmfnBO/runBO.py
@@ -2,7 +2,6 @@ import os
 import sys
 
 from bayes_opt import BayesianOptimization
-
 from bayes_opt.logger import JSONLogger
 from bayes_opt.plotter import JSONPlotter
 from bayes_opt.event import Events
@@ -21,8 +20,8 @@ if not os.path.isdir(logs):
 if not os.path.isdir(plots):
 	os.mkdir(plots)
 
-path = os.path.join(logs + str(func) + ".json")
-rename = os.path.join(plots + str(func) + ".json")
+log_name = os.path.join(logs + str(func) + ".json")
+plot_name = os.path.join(plots + str(func) + ".json")
 
 optimizer = BayesianOptimization(func, func.pbounds, random_state=1)
 
@@ -33,4 +32,4 @@ steps = 50
 optimizer.maximize(init_points=2, n_iter=0, acq='ei', kappa=5)
 optimizer.maximize(init_points=0, n_iter=steps, acq='ei', kappa=5)
 
-JSONPlotter(filename=path, rename=rename, f=str(func))
+JSONPlotter(log_name=log_name, plot_name=plot_name, func=str(func))
